db86794fe80e55f496c02cdecf3bea8ce66a3130
daniel347 <daniel.siromani@softwire.com>
2020-07-15 11:50:29 +0100

    Wrote functions to update special case items

--- csharpcore/GildedRose.cs
+++ csharpcore/GildedRose.cs
@@ -1,10 +1,16 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace csharpcore
 {
     public class GildedRose
     {
         IList<Item> Items;
+        
+        const string sulfuras = "Sulfuras, Hand of Ragnaros";
+        const string agedBrie = "Aged Brie";
+        const string backstagePasses = "Backstage passes to a TAFKAL80ETC concert";
+        
         public GildedRose(IList<Item> Items)
         {
             this.Items = Items;
@@ -14,66 +20,40 @@ namespace csharpcore
         {
             foreach (var item in Items)
             {
-                if (item.Name != "Aged Brie" && item.Name != "Backstage passes to a TAFKAL80ETC concert")
-                {
-                    if (item.Quality > 0)
-                    {
-                        if (item.Name != "Sulfuras, Hand of Ragnaros")
-                        {
-                            item.Quality -= 1;
-                        }
-                    }
-                }
-                else
-                {
-                    if (item.Quality < 50)
-                    {
-                        item.Quality += 1;
-
-                        if (item.Name == "Backstage passes to a TAFKAL80ETC concert")
-                        {
-                            if (item.SellIn < 11)
-                            {
-                                if (item.Quality < 50)
-                                {
-                                    item.Quality += 1;
-                                }
-                            }
-
-                            if (item.SellIn < 6)
-                            {
-                                if (item.Quality < 50)
-                                {
-                                    item.Quality += 1;
-                                }
-                            }
-                        }
-                    }
-                }
-
-                if (item.Name != "Sulfuras, Hand of Ragnaros")
+                if (item.Name != sulfuras)
                 {
                     item.SellIn -= 1;
                 }
+                
+                if (item.Name != agedBrie && item.Name != backstagePasses)
+                {
+                    item.Quality -= 1;
+                }
+                else
+                {
+                    item.Quality += 1;
 
+                    if (item.Name == backstagePasses)
+                    {
+                        if (item.SellIn < 11)
+                        {
+                            item.Quality += 1;
+                        }
+
+                        if (item.SellIn < 6)
+                        {
+                            item.Quality += 1;
+                        }
+                    }
+                }
+                
+                // updating sellin was here
+                
                 if (item.SellIn < 0)
                 {
-                    if (item.Name != "Aged Brie")
+                    if (item.Name != agedBrie && item.Name != backstagePasses && item.Name != sulfuras)
                     {
-                        if (item.Name != "Backstage passes to a TAFKAL80ETC concert")
-                        {
-                            if (item.Quality > 0)
-                            {
-                                if (item.Name != "Sulfuras, Hand of Ragnaros")
-                                {
-                                    item.Quality -= 1;
-                                }
-                            }
-                        }
-                        else
-                        {
-                            item.Quality = 0;
-                        }
+                        item.Quality -= 1;
                     }
                     else
                     {
@@ -84,6 +64,64 @@ namespace csharpcore
                     }
                 }
             }
+        }
+
+        public void ConstrainQualityToValidRange(Item item)
+        {
+            item.Quality = Math.Max(Math.Min(50, item.Quality), 0);
+        }
+
+        public void SetGeneralItemQuality(Item item)
+        {
+            if (item.SellIn <= 0)
+            {
+                item.Quality -= 2;
+            }
+            else
+            {
+                item.Quality -= 1;
+            }
+            
+            ConstrainQualityToValidRange(item);
+        }
+
+        public void SetAgedBrieQuality(Item item)
+        {
+            if (item.SellIn <= 0)
+            {
+                item.Quality += 2;
+            }
+            else
+            {
+                item.Quality += 1;
+            }
+            
+            ConstrainQualityToValidRange(item);
+        }
+
+        public void SetSulfurasQuality(Item item)
+        {
+            item.Quality = 80;
+        }
+
+        public void SetBackstagePassQuality(Item item)
+        {
+            if (item.SellIn <= 10)
+            {
+                item.Quality += 2;
+            }            
+            
+            if (item.SellIn <= 5)
+            {
+                item.Quality += 3;
+            }            
+            
+            if (item.SellIn <= 0)
+            {
+                item.Quality = 0;
+            }
+            
+            ConstrainQualityToValidRange(item);
         }
     }
 }
